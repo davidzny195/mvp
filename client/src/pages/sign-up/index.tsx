ffd29d461eb9803ad0284aa8db5faa50928620dc
David Zhang <david.zny1022@gmail.com>
2023-02-27 11:40:26 -0800

    download all packages

---
+++ client/src/pages/sign-up/index.tsx
@@ -0,0 +1,12 @@
+export default function signUp() {
+  return (
+    <div className="w-screen h-screen">
+      <div className="flex">
+        {/* Left */}
+        <div></div>
+        {/* Right */}
+        <div className="flex flex-col"></div>
+      </div>
+    </div>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: './client',
+  // base: '/my-project/',
+  port: 8080,
+  host: '0.0.0.0',
+  build: {
+    outDir: 'build',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  plugins: [],
+};
